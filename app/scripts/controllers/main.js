fdca50dca0abbdea9014c9d8d53e54636776a22b
magicdamo <damo@makemagic.com>
2016-03-10 11:32:55 -0800

    icons, UI work, add item, etc

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,63 @@
  * Controller of the angulartestApp
  */
 angular.module('angulartestApp')
-.controller('MainCtrl',['$scope', 'Data', function ($scope, Data) {
+.controller('MainCtrl',['$scope', '$timeout', 'Data', function ($scope, $timeout,  Data) {
+
+	this.ddv = false;
+	$scope.addlabel = '';
 
 	this.list1 = Data.getList(1);
-	console.log('list1', this.list1);
-
 	this.list2 = Data.getList(2);
-	console.log('list2', this.list2);
+
+	this.selList=null;
+
+
+	//CATCH ALL, OPERATES LIKE ESCAPE KEY FOR OPEN / ACTIVE ELEMENTS.
+	this.md = function($event) {
+		if($event.target.id==='chooser'|| $event.target.id==='caret') {
+			return;
+		}
+		this.ddv = false;
+	};
+
+	//TOGGLE THE DROPDOWN.
+	this.toggle = function() {
+		this.ddv=!this.ddv;
+	};
+
+	this.addItem = function() {
+
+		var newItem = {
+			name:$scope.addlabel,
+			id:(this.selList.length)
+		};
+
+		this.selList.push(newItem);
+		console.log('add item', this.selList);
+		$scope.addlabel = '';
+	};
+
+	this.selectList= function(n) {
+		console.log('selectList', n);
+		var self = this;
+
+		$timeout(function() { 
+			self.ddv = false;
+		}, 50);
+
+		if(!this.selList) {
+			this.selList = this.list1;
+			return;
+		}
+		if(n===2){
+			console.log('two block');
+			this.selList = this.list2;
+		} else {
+			console.log('one block');
+			this.selList = this.list1;
+		}
+		// this.selList = n===2?this.list1:this.list2;	
+		console.log('selList', this.selList[0]);
+	};
 
  }]);
--- app/scripts/factories/data.js
+++ app/scripts/factories/data.js
@@ -11,24 +11,24 @@ angular.module('angulartestApp')
 
     
     var list1=[
-        {name:'item1', id:0},
+        {name:'a item1', id:0},
         {name:'item2', id:1},
         {name:'item3', id:2},
         {name:'item4', id:3},
-        {name:'item5', id:4},
-        {name:'item6', id:5},
-        {name:'item7', id:6},
-        {name:'item8', id:7},
+        // {name:'item5', id:4},
+        // {name:'item6', id:5},
+        // {name:'item7', id:6},
+        // {name:'item8', id:7},
     ];
 
     var list2=[
-        {name:'item2.1', id:0},
+        {name:'b item2.1', id:0},
         {name:'item2.2', id:1},
         {name:'item2.3', id:2},
         {name:'item2.4', id:3},
         {name:'item2.5', id:4},
-        {name:'item2.6', id:5},
-        {name:'item2.7', id:6},
+        // {name:'item2.6', id:5},
+        // {name:'item2.7', id:6},
     ];
     
 
