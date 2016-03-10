bfaf17f49c549e733d086266df73245da0971b64
magicdamo <damo@makemagic.com>
2016-03-09 17:09:04 -0800

    initial commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angulartestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angulartestApp
+ */
+angular.module('angulartestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
